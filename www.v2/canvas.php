e7e68fd91ac7c00cdf24aaf3fc6d2189f25c2194
Daniel Costa <costa@vixinet.ch>
2015-02-08 14:18:43 +0200

    basic canvas

---
+++ www.v2/canvas.php
@@ -0,0 +1,27 @@
+<?php
+	
+	include_once('core.config.frontend.php');
+	include_once('../backend/core.config.php');
+
+	// Here we get the data from the database
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php include_once('UX.head.php'); ?>
+		<title>Déniché</title>
+	</head>
+	<body class="home">
+		<?php include_once('UX.section.header.php'); ?>
+
+		<div class="continer-fluid wrapper">
+			<div class="container">
+				<!-- Content of your page -->
+			</div>
+		</div>
+
+		<?php include_once('UX.section.footer.php'); ?>
+		<?php include_once('UX.scripts.php'); ?>
+	</body>
+</html>
