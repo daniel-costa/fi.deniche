440ef82d10d5bc9bd35fb055f52288ae4b967972
shahbibash <NintuMac@Nintus-MacBook-Pro.local>
2015-03-05 00:26:29 +0200

    checkout without s

--- www.v2/cart.checkout.php
+++ www.v2/cart.checkout.php
@@ -29,7 +29,7 @@
 						</ul>
 					</div>
 
-					<div class="col-xs-24 col-sm-12 col-md-14 col-lg-16 part-content" style=" padding-right: 3px;">
+					<div class="col-xs-24 col-sm-12 col-md-14 col-lg-16 part-content" >
 						<div class="myCart">
 
 							<div class="mycart_head">
@@ -47,9 +47,11 @@
 												$item = getProduct($id);
 												$total += $item['price'] * $amount;
 									?>
-										<tr>
+										<tr class="mycart_row">
 											<td id="img_col"><img src="<?php echo $item['thumb']; ?>"/></td>
-											<td id="qnt_col"><?php echo $amount; ?> x</td>
+											<td id="qnt_col">
+												<div class="circleBase-quantity"><?php echo $amount;?></div>
+											</td>
 											<td id="title_col">
 												<h2><?php echo $item['model']; ?></h2>
 												<h3><?php echo $item['color']; ?></h3>
@@ -75,16 +77,17 @@
 							</div>
 
 							<div class="promo">
-								<div class="promo_input" style="margin-top: 20px;">
-									<h5><?php echo $lang['Have a promo code? Enter here']; ?></h5>
+								<div class="promo_input">
+									<h5><?php echo $lang['Have a promo code? Enter here'];?></h5>
 									<div class="col-lg-6">
 										<div class="input-group">
-											<input type="text" class="form-control" >
+											<input type="text" class="form-control" />
 											<span class="input-group-btn">
-												<button class="btn btn-default" type="button"
-														style=" height: 34px; border-bottom-right-radius: 20px; border-top-right-radius: 20px;
-														background-color:rgba(242, 250, 251, 1); outline: none">+</button>
-											</span>
+												<button class="btn btn-default" type="button">
+													<span class="glyphicon glyphicon-plus" style="color:#848484"></span>
+												</button> 
+											</span>  
+											
 										</div>
 									</div>
 								</div>
@@ -95,26 +98,20 @@
 							</div>
 
 							<!-- Error message of form-->
-							<div class="error_message" style="display: none;">
-
-							</div>
-
+							<div class="error_message" style="display: none;"></div>
+							<div class="payment_details">
 							<div class="checkout_nav">
 								<ul>
 
 									<li>
 										<a class="link_nav_1" href="#" rel="shipping">  <?php echo $lang['Shipping']; ?></a>
 									</li>
-									<li>
-										<hr style="border:-1px solid #848484; width: 220px; text-align: center; margin-left: 20px; margin-right: 20px;"/>
-									</li>
+									
 
 									<li>
 										<a class="link_nav_2" href="#" rel="payment">  <?php echo $lang['Payment']; ?></a>
 									</li>
-									<li>
-										<hr style="border:-1px solid #848484; width: 200px; margin-left: 20px; margin-right: 20px; margin-top: 20px;"/>
-									</li>
+									
 									<li>
 										<a class="link_nav_3" href="#" rel="receipt">  <?php echo $lang['Receipt']; ?></a>
 									</li>
@@ -131,7 +128,7 @@
 									<input name="address2" type="text" id="address2" placeholder="<?php echo $lang['Address line 2']; ?>"/>
 									<input name="city"  type="text" id="city" placeholder="<?php echo $lang['City']; ?>" class="required"/>
 									<input name="zipcode" type="text" id="zip" placeholder="<?php echo $lang['Zip code']; ?>" class="required"/>
-									<select name="country" placeholder="Choose" style="width:280px">
+									<select name="country" placeholder="Choose" id="country">
 										<option>Finland</option>
 										<option>Sweden</option>
 										<option>Norway</option>
@@ -144,61 +141,7 @@
 
 
 							<div id="second_nav" style="display: none;">
-
-								<div class="bank_logo">
-									<a target="_blank" href="#"><img src="../www.v2/images/nordea.png" alt="nordea"></a>
-									<div class="desc">Nordea</div>
-								</div>
-
-								<div class="bank_logo">
-									<a target="_blank" href="#"><img src="../www.v2/images/osuus.png" alt="osuuspankki"></a>
-									<div class="desc">Osuuspankki</div>
-								</div>
-
-								<div class="bank_logo">
-									<a target="_blank" href="#"><img src="../www.v2/images/aktia.png" alt="aktia"></a>
-									<div class="desc">Aktia, Nooa, POP</div>
-								</div>
-
-								<div class="bank_logo">
-									<a target="_blank" href="#"><img src="../www.v2/images/ape.png" alt="ape"></a>
-									<div class="desc">Ape-Kukkaro</div>
-								</div>
-
-								<div class="bank_logo">
-									<a target="_blank" href="#"><img src="../www.v2/images/sampo.png" alt="sampo"></a>
-									<div class="desc">Sampo Pankki</div>
-								</div>
-
-								<div class="bank_logo">
-									<a target="_blank" href="#"><img src="../www.v2/images/handel.png" alt="handelsbanken"></a>
-									<div class="desc">Handelsbanken</div>
-								</div>
-
-								<div class="bank_logo">
-									<a target="_blank" href="#"><img src="../www.v2/images/visa_master.png" alt="luottokortit"></a>
-									<div class="desc">Luottokortit</div>
-								</div>
-
-								<div class="bank_logo">
-									<a target="_blank" href="#"><img src="../www.v2/images/alandsbanken.png" alt="Ålandsbanken"></a>
-									<div class="desc">Ålandsbanken</div>
-								</div>
-
-								<div class="bank_logo">
-									<a target="_blank" href="#"><img src="../www.v2/images/tapiola.png" alt="tapiola"></a>
-									<div class="desc">Tapiola Pankki</div>
-								</div>
-
-								<div class="bank_logo">
-									<a target="_blank" href="#"><img src="../www.v2/images/jousto.png" alt="juostoraha"></a>
-									<div class="desc">Juostoraha lasku ja osamasku</div>
-								</div>
-
-								<div class="bank_logo">
-									<a target="_blank" href="#"><img src="../www.v2/images/s-pankki.png" alt="s-panki"></a>
-									<div class="desc">S-Pankki</div>
-								</div>
+								<h1>payment</h1>
 
 							</div>
 
@@ -206,6 +149,7 @@
 								<h1>Receipt</h1>
 
 							</div>
+						</div>
 						</div>
 					</div>
 				</div>
@@ -220,10 +164,8 @@
 			$('.item_remove').click(function (e) {
 
 				$(this).closest('tr').remove();
-				//$('.total_price').html("<?php echo $mycart_total_price . "&euro;" ?>");
 				return false;
-				 e.preventDefault();
-
+				e.preventDefault();
 			});
 		 
 
@@ -237,6 +179,7 @@
 				e.preventDefault();
 			});
 			$(".link_nav_2").on('click', function (e) {
+				validateForm();
 				$("#first_nav").css("display", "none");
 				$("#third_nav").css("display", "none");
 				$("#second_nav").css("display", "inline");
@@ -250,6 +193,7 @@
 				return false;
 				e.preventDefault();
 			});
+
 			function validateForm() {
 				var all_fields = document.getElementById('first_name', 'last_name', 'address1', 'city', 'zip');
 				var firstname = document.getElementById('first_name');
@@ -290,9 +234,7 @@
 				} else {
 					$(".error_message").css("display", "none");
 				}
-
 			}
-
 		</script>
 	</body>
 </html>
